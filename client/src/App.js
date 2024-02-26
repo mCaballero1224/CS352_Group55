1cf2ce3c8f55b0943d0601d66a14a0e6bc48766b
Michael Caballero <mcaballero.dev@gmail.com>
2024-02-26 10:18:26 -0800

    update index.html w/ new favicon images

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+/* IMPORTS */
+// npm packages
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+// stylesheet
 import './App.css';
+//components
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/home" element={<Home/>}/>
+        </Routes>
+      </Router>
+      <p id="copyright">&copy; Future Gadget Team (2024)</p>
     </div>
   );
 }
